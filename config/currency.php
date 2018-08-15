053331d129c48cb00ab5d4093d6aff12748bb2b2
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-15 12:17:27 +0800

    Added default swift code key

--- config/currency.php
+++ config/currency.php
@@ -2,6 +2,7 @@
 
 return [
     'default' => env('CURRENCY_DEFAULT', 'MY'),
+    'default_swift_code' => env('CURRENCY_DEFAULT_SWIFT_CODE', 'MYR'),
     'MY'      => [
         'swift_code' => 'MYR',
         'symbol'     => 'RM',
